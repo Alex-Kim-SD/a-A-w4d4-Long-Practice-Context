5c4560452c4ead9d1a138dbbe98c697051b3daa4
alex5 <alex@kimnetics.com>
2023-01-19 12:28:50 -1000

    07 completed ()

--- problems/07-all-the-args.js
+++ problems/07-all-the-args.js
@@ -1,6 +1,16 @@
+/****************************************************************
+Implement the following in the problems/07-all-the-args.js file.
+
+Write a function named allTheArgs(func, ...args) that will accept a function func
+and any number of arguments. The allTheArgs function should return the passed-in
+function func modified to always be invoked with the
+passed-in arguments args. See code block below for further details.
+********************************************************************/
+
 function allTheArgs(func, ...args) {
-  // Your code here
-}
+    const newFunc = func.bind(null,...args)
+    return newFunc
+  }
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
