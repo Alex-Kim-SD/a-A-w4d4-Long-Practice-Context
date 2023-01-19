df2daa1459f07af09c5e1e3f8b6b64da29ea4069
Alan Nguyen <alanngn21@gmail.com>
2023-01-19 15:19:56 -0800

    10 done

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -10,7 +10,32 @@ If there are no guests, return "Gotta add people to the guest list"
 If there are guests in the guestList, return the guests' names. For example,
 if there are two guests, return "Welcome to the party ${name1} and ${name2}".
 ***********************************************************/
+class PartyPlanner{
+	constructor(guestList=[]){
+		this.guestList = guestList
+	}
+	addToGuestList(name){
+		this.guestList.push(name)
+		return
+	}
+	throwParty(){
+		if(this.guestList.length ===0 ){return `Gotta add people to the guest list`}
 
+		return `Welcome to the party ${this.guestList.join(' and ')}`
+		// return this.guestList.forEach(ele=> `Welcome to the party ${ele}`)
+		// if(this.guestList.length ===1){return `Welcome to the party ${this.guestList[0]}`}
+
+	}
+}
+const party = new PartyPlanner();
+
+console.log(party.throwParty()); // prints "Gotta add people to the guest list"
+
+party.addToGuestList("James");
+console.log(party.throwParty()); // prints "Welcome to the party James"
+
+party.addToGuestList("Alvin");
+console.log(party.throwParty()); // prints "Welcome to the party James and Alvin"
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
