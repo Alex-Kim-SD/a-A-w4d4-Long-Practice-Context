9e9d2eded501d7eada3f9f151f0267d76681fe9c
alex5 <alex@kimnetics.com>
2023-01-19 12:54:42 -1000

    09 done

--- problems/09-call-on-target.js
+++ problems/09-call-on-target.js
@@ -1,7 +1,20 @@
-function callOnTarget(func, obj1, obj2) {
-  // Your code here
-}
+/***************************************************************
+Implement the following in the problems/09-call-on-target.js file.
 
+Write a function named callOnTarget(func, obj1, obj2) that
+will accept a function func and two objects, obj1 and obj2.
+callOnTarget should return thev result of the function func
+invoked with obj1 as its context and obj2 as the first argument.
+****************************************************************/
+
+// function callOnTarget(func, obj1, obj2) {
+//   const newFunc = func.bind(obj1, obj2)
+//   return newFunc()
+// }
+// // both above and below work
+function callOnTarget(func, obj1, obj2) {
+  return func.call(obj1, obj2)
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,15 @@
-// Your code here
+/************************************************************
+Implement the following in the problems/10-party-planner.js file.
 
+Create a PartyPlanner class.
+A newly instantiated instance should have its guestList property initialized to an empty array.
+Add two instance methods:
+a. addToGuestList(name) - add the name to the guestList
+b. throwParty() - return a different string depending on the length of the guestList:
+If there are no guests, return "Gotta add people to the guest list"
+If there are guests in the guestList, return the guests' names. For example,
+if there are two guests, return "Welcome to the party ${name1} and ${name2}".
+***********************************************************/
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
